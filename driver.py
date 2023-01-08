5ac1fe193ad76bb602575b882d160a590a7f3375
kjw8118 <kjw8118@gmail.com>
2023-01-08 16:27:48 +0900

    Driver 모듈 내 회전변환 Ursina 내장 함수 사용

--- driver.py
+++ driver.py
@@ -36,7 +36,7 @@ class Driver(Vehicle):
 
     def get_driver_input(self):
         self.pygame.event.get()
-        self.aps = (self.pad.get_axis(5) + 1)/2/4
+        self.aps = (self.pad.get_axis(5) + 1)/2
         self.bps = (self.pad.get_axis(4) + 1)/2
         self.steering = self.pad.get_axis(0)/3
 
@@ -50,17 +50,13 @@ class Driver(Vehicle):
 
     def camera_following(self):
         if self.is_ortho:
-            follow = self.rotation_transformation(self.rotation_y * math.pi / 180 + self.steering / 3, [0, 10])
+            follow = ursinamath.rotate_point_2d(Vec2(0, 10), Vec2(0, 0), self.rotation_y + self.steering * 180 / math.pi / 3)
             camera.world_position += (self.world_position + Vec3(follow[0], 7.5, follow[1]) - camera.world_position) * 0.035
             camera.rotation_y += (self.rotation_y + self.steering * 180 / math.pi / 3 - camera.rotation_y) * 0.02
         else:
-            follow = self.rotation_transformation(self.rotation_y * math.pi / 180 + self.steering / 3, [0, -20])
+            follow = ursinamath.rotate_point_2d(Vec2(0, -20), Vec2(0, 0), self.rotation_y + self.steering * 180 / math.pi / 3)
             camera.world_position += (self.world_position + Vec3(follow[0], 7.5, follow[1]) - camera.world_position) * 0.035
             camera.rotation_y += (self.rotation_y + self.steering*180/math.pi/3 - camera.rotation_y) * 0.035
-
-    def rotation_transformation(self, angle, vec1):
-        vec_np = np.array([[vec1[0]], [vec1[1]]])
-        return np.array([[math.cos(-angle), -math.sin(-angle)],[math.sin(-angle), math.cos(-angle)]])@vec_np
 
 
 
@@ -75,10 +71,10 @@ if __name__=="__main__":
     random.seed(0)
     Entity.default_shader = lit_with_shadows_shader
 
-    ground = Entity(model='plane', collider='box', scale=512, texture='grass', texture_scale=(24, 24), name='ground')
-    target = Vehicle(z=50)
+    ground = Entity(model='plane', collider='box', scale=1024, texture='grass', texture_scale=(24, 24), name='ground')
+    target = Vehicle(z=-40, x=10)
 
-    player = Driver(z=-50)
+    player = Driver(z=-20)
     ortho_bool = False
 
     def update():
