dabe3cfe468a8ce54f64d5cddb74acf4bc638f53
kjw8118 <kjw8118@gmail.com>
2023-01-08 14:51:55 +0900

    Vehicle 모듈과 Driver 모듈간 종속관계 정리를 위해 Dynamics 모듈 이동

--- driver.py
+++ driver.py
@@ -2,91 +2,16 @@ from ursina import *
 
 import math
 
-from vehicle import Vehicle
+from vehicle import Vehicle, VehicleDynamics, Wheel
 import numpy as np
 
-class VehicleModel:
 
-    def __init__(self, position=np.array([[0],[0]]), rotation=0, wheelbase=4, track=1.8):
-        self.position = position
-        self.rotation = rotation/180*math.pi
-        self.wheelbase = wheelbase
-        self.track = track
-        self.outline = {}
-        self.update_outline()
-
-        self.is_first = True
-        self.last_value = 0
-        self.num_phi = rotation/180*math.pi
-
-    def update_position(self, movement, steering):
-        if steering == 0:
-            self.position += self.rotation_transformation(self.rotation, [0, movement])
-            current_value = 0
-        else:
-            corner_radius = self.wheelbase / steering
-            corner_angle = movement / corner_radius
-            self.rotation += corner_angle
-            local_increament = self.rotation_transformation(corner_angle, [-corner_radius, 0]) + np.array([[corner_radius], [0]])
-            self.position += self.rotation_transformation(self.rotation,local_increament.reshape(1,2).tolist()[0])
-            current_value = steering*movement/(2*self.wheelbase)
-        if self.is_first:
-            self.last_value = current_value
-            self.is_first = False
-        self.num_phi += current_value + self.last_value
-        self.last_value = current_value
-        print(self.num_phi, self.rotation,";")
-        self.update_outline()
-
-    def update_outline(self):
-        self.outline['RL'] = self.rotation_transformation(self.rotation, [-self.track / 2, -self.wheelbase/2]) + self.position
-        self.outline['RR'] = self.rotation_transformation(self.rotation, [self.track / 2, -self.wheelbase/2]) + self.position
-        self.outline['FL'] = self.rotation_transformation(self.rotation, [-self.track / 2, self.wheelbase/2]) + self.position
-        self.outline['FR'] = self.rotation_transformation(self.rotation, [self.track / 2, self.wheelbase/2]) + self.position
-
-    def get_position(self):
-        return self.position
-
-    def get_rotation(self):
-        return self.rotation*180/math.pi
-
-    def rotation_transformation(self, angle, vec1):
-        vec_np = np.array([[vec1[0]], [vec1[1]]])
-        return np.array([[math.cos(-angle), -math.sin(-angle)],[math.sin(-angle), math.cos(-angle)]])@vec_np
-
-class Wheel:
-
-    def __init__(self, driver):
-        self.left_wheel = Entity(model=Cylinder(resolution=16, radius=0.5, start=0.05, height=0.2, color_gradient=[color.black, color.gray]), y=0.5, rotation_x=-90, rotation_y=90)
-        self.right_wheel = Entity(model=Cylinder(resolution=16, radius=0.5, start=0.05, height=0.2, color_gradient=[color.black, color.gray]), y=0.5, rotation_x=-90, rotation_y=-90)
-        self.rear_left_wheel = Entity(model=Cylinder(resolution=16, radius=0.5, start=0.05, height=0.2, color_gradient=[color.black, color.gray]), y=0.5, rotation_x=-90, rotation_y=90)
-        self.rear_right_wheel = Entity(model=Cylinder(resolution=16, radius=0.5, start=0.05, height=0.2, color_gradient=[color.black, color.gray]), y=0.5, rotation_x=-90, rotation_y=-90)
-        self.parent = driver
-
-    def update(self):
-        self.left_wheel.world_position = Vec3(self.parent.vehicle_model.outline['FL'][0], self.left_wheel.world_position.y, self.parent.vehicle_model.outline['FL'][1])
-        self.left_wheel.rotation_y = 90 + self.parent.rotation_y + self.parent.steering * 180 / math.pi
-        self.right_wheel.world_position = Vec3(self.parent.vehicle_model.outline['FR'][0], self.left_wheel.world_position.y,
-                                                self.parent.vehicle_model.outline['FR'][1])
-        self.right_wheel.rotation_y = -90 + self.parent.rotation_y + self.parent.steering * 180 / math.pi
-
-        self.rear_left_wheel.world_position = Vec3(self.parent.vehicle_model.outline['RL'][0],
-                                              self.left_wheel.world_position.y,
-                                              self.parent.vehicle_model.outline['RL'][1])
-        self.rear_left_wheel.rotation_y = 90 + self.parent.rotation_y
-        self.rear_right_wheel.world_position = Vec3(self.parent.vehicle_model.outline['RR'][0],
-                                               self.left_wheel.world_position.y,
-                                               self.parent.vehicle_model.outline['RR'][1])
-        self.rear_right_wheel.rotation_y = -90 + self.parent.rotation_y
-
-
-class Driver(Entity):
+class Driver(Vehicle):
     import pygame
 
     def __init__(self, **kwargs):
-        super().__init__(model='cube', collider='box', scale_x=1.8, scale_y=0.8, scale_z=4, origin_y=-1, **kwargs)
-        self.vehicle_model = VehicleModel(position=np.array([[self.world_position.x], [self.world_position.z]]), rotation=self.rotation_y)
-        self.wheel = Wheel(self)
+        super().__init__(**kwargs)
+
 
         self.is_ortho = False
         camera.rotation_x = 12
@@ -102,17 +27,12 @@ class Driver(Entity):
         self.power = 0
 
     def update(self):
-        #print(self.world_position)
+
         self.get_driver_input()
-        self.vehicle_model.update_position(self.aps, self.steering)
-        position = self.vehicle_model.get_position()
-        rotation = self.vehicle_model.get_rotation()
-        #print(position, rotation)
-        self.world_position = Vec3(position[0], self.world_position.y, position[1])
-        self.rotation_y = rotation
+        self.update_dynamics(self.aps, self.steering*180/math.pi)
+
         self.camera_following()
-        self.wheel.update()
-        #pass
+
 
     def get_driver_input(self):
         self.pygame.event.get()
--- vehicle.py
+++ vehicle.py
@@ -1,14 +1,115 @@
 from ursina import *
 
 import math
+import numpy as np
+
+
+class Wheel:
+    def __init__(self, dynamics):
+        self.left_wheel = Entity(
+            model=Cylinder(resolution=16, radius=0.5, start=0.05, height=0.2, color_gradient=[color.black, color.gray]),
+            y=0.5, rotation_x=-90, rotation_y=90)
+        self.right_wheel = Entity(
+            model=Cylinder(resolution=16, radius=0.5, start=0.05, height=0.2, color_gradient=[color.black, color.gray]),
+            y=0.5, rotation_x=-90, rotation_y=-90)
+        self.rear_left_wheel = Entity(
+            model=Cylinder(resolution=16, radius=0.5, start=0.05, height=0.2, color_gradient=[color.black, color.gray]),
+            y=0.5, rotation_x=-90, rotation_y=90)
+        self.rear_right_wheel = Entity(
+            model=Cylinder(resolution=16, radius=0.5, start=0.05, height=0.2, color_gradient=[color.black, color.gray]),
+            y=0.5, rotation_x=-90, rotation_y=-90)
+        self.dynamics = dynamics
+
+    def update(self):
+        self.left_wheel.world_position = Vec3(self.dynamics.outline['FL'][0], self.left_wheel.world_position.y,
+                                              self.dynamics.outline['FL'][1])
+        self.left_wheel.rotation_y = 90 + self.dynamics.get_rotation() + self.dynamics.get_steering()
+
+        self.right_wheel.world_position = Vec3(self.dynamics.outline['FR'][0], self.left_wheel.world_position.y,
+                                               self.dynamics.outline['FR'][1])
+        self.right_wheel.rotation_y = -90 + self.dynamics.get_rotation() + self.dynamics.get_steering()
+
+        self.rear_left_wheel.world_position = Vec3(self.dynamics.outline['RL'][0], self.left_wheel.world_position.y,
+                                                   self.dynamics.outline['RL'][1])
+        self.rear_left_wheel.rotation_y = 90 + self.dynamics.get_rotation()
+
+        self.rear_right_wheel.world_position = Vec3(self.dynamics.outline['RR'][0], self.left_wheel.world_position.y,
+                                                    self.dynamics.outline['RR'][1])
+        self.rear_right_wheel.rotation_y = -90 + self.dynamics.get_rotation()
+
+class VehicleDynamics:
+
+    def __init__(self, position=np.array([[0],[0]]), rotation=0, wheelbase=4, track=1.8):
+        self.position = position
+        self.rotation = rotation/180*math.pi
+        self.wheelbase = wheelbase
+        self.track = track
+        self.steering = 0
+
+        self.outline = {}
+        self.__update_outline()
+
+        self.is_first = True
+        self.last_value = 0
+        self.num_phi = rotation/180*math.pi
+
+    def update_position(self, movement, steering):
+        self.steering = steering/180*math.pi
+        if self.steering == 0:
+            self.position += self.__rotation_transformation(self.rotation, [0, movement])
+            current_value = 0
+        else:
+            corner_radius = self.wheelbase / self.steering
+            corner_angle = movement / corner_radius
+            self.rotation += corner_angle
+            local_increament = self.__rotation_transformation(corner_angle, [-corner_radius, 0]) + np.array([[corner_radius], [0]])
+            self.position += self.__rotation_transformation(self.rotation,local_increament.reshape(1,2).tolist()[0])
+            current_value = self.steering*movement/(2*self.wheelbase)
+        if self.is_first:
+            self.last_value = current_value
+            self.is_first = False
+        self.num_phi += current_value + self.last_value
+        self.last_value = current_value
+        self.__update_outline()
+
+    def __update_outline(self):
+        self.outline['RL'] = self.__rotation_transformation(self.rotation, [-self.track / 2, -self.wheelbase/2]) + self.position
+        self.outline['RR'] = self.__rotation_transformation(self.rotation, [self.track / 2, -self.wheelbase/2]) + self.position
+        self.outline['FL'] = self.__rotation_transformation(self.rotation, [-self.track / 2, self.wheelbase/2]) + self.position
+        self.outline['FR'] = self.__rotation_transformation(self.rotation, [self.track / 2, self.wheelbase/2]) + self.position
+
+    def get_position(self):
+        return self.position
+
+    def get_rotation(self):
+        return self.rotation*180/math.pi
+
+    def get_steering(self):
+        return self.steering*180/math.pi
+
+    def __rotation_transformation(self, angle, vec1):
+        vec_np = np.array([[vec1[0]], [vec1[1]]])
+        return np.array([[math.cos(-angle), -math.sin(-angle)],[math.sin(-angle), math.cos(-angle)]])@vec_np
+
 
 class Vehicle(Entity):
     def __init__(self, **kwargs):
-        try:
-            super().__init__(model='assets/Car.obj', texture=None, collider='mesh', origin_y=0, color=color.orange, **kwargs)
-        except:
-            super().__init__(model='Box', texture=None, collider='mesh', origin_y=0, color=color.orange,
+        if False:
+            super().__init__(model='assets/Car.obj', texture=None, collider='box', scale_x=1.8, scale_y=0.8, scale_z=4, origin_y=-1, color=color.orange, **kwargs)
+        else:
+            super().__init__(model='cube', texture=None, collider='box', scale_x=1.8, scale_y=0.8, scale_z=4, origin_y=-1, color=color.green,
                              **kwargs)
+        self.dynamics = VehicleDynamics(position=np.array([[self.world_position.x], [self.world_position.z]]),
+                                             rotation=self.rotation_y)
+        self.wheel = Wheel(self.dynamics)
 
     def update(self):
-        pass
+        self.update_dynamics(0, 0)
+    def update_dynamics(self, aps, steering):
+        self.dynamics.update_position(aps, steering)
+        position = self.dynamics.get_position()
+        rotation = self.dynamics.get_rotation()
+        self.world_position = Vec3(position[0], self.world_position.y, position[1])
+        self.rotation_y = rotation
+
+        self.wheel.update()
